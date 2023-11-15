d40062a22b1e5dc6ad6fb171ae2808a352e8cf1a
michaelm9267 <95662360+michaelm9267@users.noreply.github.com>
2023-11-15 16:46:35 -0600

    finished getting the cards set up

--- squad-deck/src/pages/home-page.js
+++ squad-deck/src/pages/home-page.js
@@ -11,11 +11,11 @@ const HomePage = () => {
   const { user } = useAuth0();
   const ref = useRef();
 
-  console.log(user);
+//   console.log(user);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/AlphaRoster.csv')
+     fetch('/AlphaRoster.csv')
         .then(response => response.text())
         .then(text => {
             Papa.parse(text, {
@@ -33,31 +33,34 @@ const HomePage = () => {
     <>
       <PageLayout>
         <h1>This is the home page.</h1>
+        <div className='cardCnt' style={{display: 'flex', flexWrap: 'wrap', gap: '15px' }}>
         {data.map((el) => {
+            return (
         <Flippy
           flipOnHover={false}
           flipOnClick={true}
           flipDirection="horizontal"
           ref={ref}
-          style={{ width: "auto", height: "auto", maxWidth: "300px" }}>
-          <FrontSide style={{ backgroundColor: "#41669d", borderRadius: "25px", padding:'0', margin: "0 auto", textAlign: "center" }}>
+          style={{ width: "15vw", height: "35vh", maxWidth: ""}}>
+          <FrontSide style={{ backgroundColor: "#41669d", borderRadius: "25px", padding:'0', margin: "0 auto", textAlign: "center"}}>
           <div className='cdHdr' style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
-          <img src="" alt="rank" />
-          <h4>{data.FULL_NAME}</h4>
-          <p>YOS</p>
+          <p>{el.GRADE}</p>
+          <h4>{el.FULL_NAME}</h4>
+          <p>{el.DOE}</p>
           </div>
           <img src="" alt='Profile Picture' />
-          <h4>Job Selection</h4>
-          <p>AFSC</p>
+          <h4>{el.DUTY_TITLE}</h4>
+          <p>{el.CAFSC}</p>
           <h4>Supervisor:</h4>
-          <p>MSgt Supervisor</p>
+          <p>{el.SUPV_NAME}</p>
           <img src="" alt="Achievment images" />
           </FrontSide>
           <BackSide style={{ backgroundColor: "#175852", borderRadius: "25px" }}>
 
           </BackSide>
-        </Flippy> })}
-
+              </Flippy>
+)})}
+    </div> 
         <Dropzone />
       </PageLayout>
     </>
