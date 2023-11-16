3aa102910b41f1f16f094923a67c815b78c0e05f
michaelm9267 <95662360+michaelm9267@users.noreply.github.com>
2023-11-16 16:13:42 -0600

    no message

--- squad-deck/src/pages/home-page.js
+++ squad-deck/src/pages/home-page.js
@@ -3,6 +3,7 @@ import PageLayout from "../components/page-layout";
 import { useAuth0 } from "@auth0/auth0-react";
 import Dropzone from "../components/Dropzone";
 import Flippy, { FrontSide, BackSide } from "react-flippy";
+import "../Styled/home-page.css"
 import { Link } from "react-router-dom";
 import Papa from 'papaparse';
 
@@ -15,25 +16,19 @@ const HomePage = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-     fetch('/AlphaRoster.csv')
-        .then(response => response.text())
-        .then(text => {
-            Papa.parse(text, {
-                header: true,
-                complete: results => {
-                    setData(results.data);
-                }
-            });
-        });
+     fetch('http://localhost:8080/roster')
+        .then(response => response.json())
+        .then(data => {
+                    setData(data);
+                })
 }, []);
 
-
+console.log(data)
 
   return (
     <>
       <PageLayout>
-        <h1>This is the home page.</h1>
-        <div className='cardCnt' style={{display: 'flex', flexWrap: 'wrap', gap: '15px' }}>
+        <div className='cardCnt'>
         {data.map((el) => {
             return (
         <Flippy
@@ -41,21 +36,21 @@ const HomePage = () => {
           flipOnClick={true}
           flipDirection="horizontal"
           ref={ref}
-          style={{ width: "15vw", height: "35vh", maxWidth: ""}}>
-          <FrontSide style={{ backgroundColor: "#41669d", borderRadius: "25px", padding:'0', margin: "0 auto", textAlign: "center"}}>
-          <div className='cdHdr' style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
-          <p>{el.GRADE}</p>
-          <h4>{el.FULL_NAME}</h4>
-          <p>{el.DOE}</p>
+          className="FlippyCard">
+          <FrontSide className="FrontSide">
+          <div className='cdHdr'>
+          <p>{el.grade}</p>
+          <h4>{el.full_name}</h4>
+          <p>In the past</p>
           </div>
           <img src="" alt='Profile Picture' />
-          <h4>{el.DUTY_TITLE}</h4>
-          <p>{el.CAFSC}</p>
+          <h4>{el.duty_title}</h4>
+          <p>{el.dafsc}</p>
           <h4>Supervisor:</h4>
-          <p>{el.SUPV_NAME}</p>
+          <p>MSgt Supervisor</p>
           <img src="" alt="Achievment images" />
           </FrontSide>
-          <BackSide style={{ backgroundColor: "#175852", borderRadius: "25px" }}>
+          <BackSide className="BackSide">
 
           </BackSide>
               </Flippy>
