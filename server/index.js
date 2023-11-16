c2365fb1aa939aaa0d42420600accd70e7b62cca
officeraggro <countringworm@gmail.com>
2023-11-16 16:03:57 -0600

    Added fetch full roster by unit route to Express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const knex = require("knex")(require("./knexfile")["development"]);
 const cors = require('cors')
 const { getFullRoster, getPersonnelDetails } = require("./controller");
+
 
 const port = 8080
 
@@ -13,6 +15,7 @@ app.get('/', (req, res) => {
     res.status(200).send('This here is the root route!')
 })
 
+// Route for testing purposes
 app.get('/roster', (req, res) => {
 
     try {
@@ -28,6 +31,7 @@ app.get('/roster', (req, res) => {
     }
 })
 
+// Route for testing purposes
 app.get("/personnel", (req, res) => {
   try {
     getPersonnelDetails()
@@ -41,6 +45,41 @@ app.get("/personnel", (req, res) => {
     res.status(500).send("Error: ", error);
   }
 });
+
+// Fetch full roster and personnel details by unit
+app.get("/units/:id/roster", async (req, res) => {
+  const { id } = req.params
+  
+  try {
+    const unitObj = await knex('units').select('*').where('units.id', '=', id)
+
+    const workcentersObj = await knex('workcenters').select('*').where('workcenters.workcenter_unit_id', '=', unitObj[0].id)
+
+    const alphaRosterObj = await knex("units_alpha_roster AS uar")
+      .join("alpha_roster AS ar", "ar.id", "=", "uar.alpha_roster_id")
+      .join("personnel_details AS pd", "pd.alpha_roster_id", "=", "ar.id")
+      .select("ar.*", "pd.id AS personnel_id", "pd.go_by", "pd.favorite_movie", "pd.hobbies", "pd.achievements", "pd.spouse_name", "pd.children_num", "pd.children_names", "pd.personal_img", "pd.grade_emblem_img", "pd.achievement_imgs", "pd.interesting_fact")
+      .where("uar.unit_id", "=", unitObj[0].id);
+
+    
+
+    const unitAlphaRosterObj = {
+      id: unitObj[0].id,
+      unit_full_name: unitObj[0].unit_full_name,
+      unit_abbr: unitObj[0].unit_abbr,
+      unit_parent: unitObj[0].unit_parent,
+      unit_emblem_url: unitObj[0].unit_emblem_url,
+      unit_station: unitObj[0].unit_station,
+      workcenters: workcentersObj,
+      alpha_roster: alphaRosterObj,
+    }
+
+    res.status(200).send(unitAlphaRosterObj)
+
+  } catch (err) {
+    res.status(404).send("Page not found")
+  }
+})
 
 
 
--- squad-deck/src/pages/home-page.js
+++ squad-deck/src/pages/home-page.js
@@ -1,66 +1,108 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 import PageLayout from "../components/page-layout";
 import { useAuth0 } from "@auth0/auth0-react";
 import Dropzone from "../components/Dropzone";
 import Flippy, { FrontSide, BackSide } from "react-flippy";
 import { Link } from "react-router-dom";
-import Papa from 'papaparse';
+import Papa from "papaparse";
 
 const HomePage = () => {
-
   const { user } = useAuth0();
   const ref = useRef();
 
-//   console.log(user);
+  console.log(user);
   const [data, setData] = useState([]);
 
+  //   useEffect(() => {
+  //      fetch('/AlphaRoster.csv')
+  //         .then(response => response.text())
+  //         .then(text => {
+  //             Papa.parse(text, {
+  //                 header: true,
+  //                 complete: results => {
+  //                     setData(results.data);
+  //                 }
+  //             });
+  //         });
+  // }, []);
+
   useEffect(() => {
-     fetch('/AlphaRoster.csv')
-        .then(response => response.text())
-        .then(text => {
-            Papa.parse(text, {
-                header: true,
-                complete: results => {
-                    setData(results.data);
-                }
-            });
-        });
-}, []);
+    fetch("http://localhost:8080/units/1/roster")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
-
+  // console.log(data.workcenters)
 
   return (
     <>
       <PageLayout>
         <h1>This is the home page.</h1>
-        <div className='cardCnt' style={{display: 'flex', flexWrap: 'wrap', gap: '15px' }}>
-        {data.map((el) => {
+        <div
+          className="cardCnt"
+          style={{ display: "flex", flexWrap: "wrap", gap: "15px" }}
+        >
+          {data?.alpha_roster?.map((el, indx) => {
             return (
-        <Flippy
-          flipOnHover={false}
-          flipOnClick={true}
-          flipDirection="horizontal"
-          ref={ref}
-          style={{ width: "15vw", height: "35vh", maxWidth: ""}}>
-          <FrontSide style={{ backgroundColor: "#41669d", borderRadius: "25px", padding:'0', margin: "0 auto", textAlign: "center"}}>
-          <div className='cdHdr' style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
-          <p>{el.GRADE}</p>
-          <h4>{el.FULL_NAME}</h4>
-          <p>{el.DOE}</p>
-          </div>
-          <img src="" alt='Profile Picture' />
-          <h4>{el.DUTY_TITLE}</h4>
-          <p>{el.CAFSC}</p>
-          <h4>Supervisor:</h4>
-          <p>{el.SUPV_NAME}</p>
-          <img src="" alt="Achievment images" />
-          </FrontSide>
-          <BackSide style={{ backgroundColor: "#175852", borderRadius: "25px" }}>
-
-          </BackSide>
+              <Flippy
+                flipOnHover={false}
+                flipOnClick={true}
+                flipDirection="horizontal"
+                ref={ref}
+                key={indx}
+                style={{ width: "15vw", height: "50vh", maxWidth: "" }}
+              >
+                <FrontSide
+                  style={{
+                    backgroundColor: "#41669d",
+                    borderRadius: "25px",
+                    padding: "0",
+                    margin: "0 auto",
+                    textAlign: "center",
+                  }}
+                >
+                  <div
+                    className="cdHdr"
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-around",
+                      alignItems: "center",
+                    }}
+                  >
+                    <p>{el.grade}</p>
+                    <h4>{el.full_name}</h4>
+                    <p>{el.doe}</p>
+                  </div>
+                  <h4>{el.go_by}</h4>
+                  <img
+                    src={el.personal_img}
+                    alt={el.full_name + "profile picture"}
+                    height="100px"
+                  />
+                  <h4>{el.duty_title}</h4>
+                  <p>{el.cafsc}</p>
+                  <h4>Supervisor:</h4>
+                  <p>{el.supv_name}</p>
+                  <img
+                    src={el.achievement_imgs}
+                    alt={el.full_name + "achievements"}
+                    height="25px"
+                  />
+                </FrontSide>
+                <BackSide
+                  style={{ backgroundColor: "#175852", borderRadius: "25px" }}
+                >
+                  <h4>Favorite Movie</h4>
+                  <p>{el.favorite_movie}</p>
+                  <h4>Hobbies</h4>
+                  <p>{el.hobbies}</p>
+                  <h4>Interesting Fact</h4>
+                  <p>{el.interesting_fact}</p>
+                </BackSide>
               </Flippy>
-)})}
-    </div> 
+            );
+          })}
+        </div>
         <Dropzone />
       </PageLayout>
     </>
