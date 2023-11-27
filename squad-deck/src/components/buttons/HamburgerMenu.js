529a379448f1ff8299a35990dbb3ba575a91b83f
loi1 <pham.loi51@gmail.com>
2023-11-27 08:59:27 -0600

    hamburger changes

--- squad-deck/src/components/buttons/HamburgerMenu.js
+++ squad-deck/src/components/buttons/HamburgerMenu.js
@@ -5,7 +5,12 @@ import { SdUserContext } from "../sd-user-context";
 import "../../Styled/nav-bar.css";
 import styled from "styled-components";
 import { Layout, Menu, Button, theme } from "antd";
-import { HomeOutlined, BarsOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import {
+	HomeOutlined,
+	BarsOutlined,
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+} from "@ant-design/icons";
 
 const { Header, Sider } = Layout;
 
@@ -35,8 +40,8 @@ const HamburgerMenu = () => {
 									returnTo: "http://localhost:3001/",
 								},
 							});
-						} else if (key === 'menu' || key === 'toggle') {
-							console.log('Menu clicked!')
+						} else if (key === "menu" || key === "toggle") {
+							console.log("Menu clicked!");
 						} else {
 							navigate(key);
 						}
@@ -55,9 +60,9 @@ const HamburgerMenu = () => {
 					<Menu.SubMenu key="menu" icon={<BarsOutlined />} title="Menu">
 						<Menu.Item key="/org">Org Chart</Menu.Item>
 						<Menu.Item key="/upload">Upload Roster</Menu.Item>
-						<Menu.Item key="signout">Sign Out</Menu.Item>
-						<Menu.Item key="/settings">Settings</Menu.Item>
 					</Menu.SubMenu>
+					<Menu.Item key="signout">Sign Out</Menu.Item>
+					<Menu.Item key="/settings">Settings</Menu.Item>
 				</Menu>
 			</Sider>
 		</Layout>
