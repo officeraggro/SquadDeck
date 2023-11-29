7ba7b30b9335b2ac4a31ed7d58a16ff558e4b9a8
loi1 <pham.loi51@gmail.com>
2023-11-29 14:09:52 -0600

    lotta setting page changes

--- squad-deck/src/App.js
+++ squad-deck/src/App.js
@@ -16,72 +16,77 @@ import SearchContext from "./components/SearchContext";
 import { RosterUploadContext } from "./components/roster-upload-context";
 import ImageUploadTest from "./pages/image-upload-test";
 import UploadPage from "./pages/upload-page";
+import Settings from "./pages/settings";
 
 const App = () => {
-  const [sdUser, setSdUser] = useState([]);
-  const [searchData, setSearchData] = useState([]);
-  const [rosterUpload, setRosterUpload] = useState(false);
-  const [data, setData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const { isLoading } = useAuth0();
+	const [sdUser, setSdUser] = useState([]);
+	const [searchData, setSearchData] = useState([]);
+	const [rosterUpload, setRosterUpload] = useState(false);
+	const [data, setData] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+	const { isLoading } = useAuth0();
 
-  if (isLoading) {
-    return (
-      <div>
-        <PageLoader size="100px" />
-      </div>
-    );
-  }
+	if (isLoading) {
+		return (
+			<div>
+				<PageLoader size="100px" />
+			</div>
+		);
+	}
 
-  return (
-    <>
-      <SdUserContext.Provider value={{ sdUser, setSdUser }}>
-        <SearchContext.Provider
-          value={{
-            data,
-            setData,
-            searchTerm,
-            setSearchTerm,
-            searchData,
-            setSearchData,
-          }}
-        >
-          <RosterUploadContext.Provider
-            value={{ rosterUpload, setRosterUpload }}
-          >
-            <Routes>
-              {/* public */}
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/onboarding" element={<UnitOnboardingPage />} />
+	return (
+		<>
+			<SdUserContext.Provider value={{ sdUser, setSdUser }}>
+				<SearchContext.Provider
+					value={{
+						data,
+						setData,
+						searchTerm,
+						setSearchTerm,
+						searchData,
+						setSearchData,
+					}}
+				>
+					<RosterUploadContext.Provider
+						value={{ rosterUpload, setRosterUpload }}
+					>
+						<Routes>
+							{/* public */}
+							<Route path="/" element={<LandingPage />} />
+							<Route path="/signup" element={<SignupPage />} />
+							<Route path="/onboarding" element={<UnitOnboardingPage />} />
 
-              {/* private */}
-              <Route path="/home" element={<HomePage />} />
-              <Route
-                path="/org"
-                element={<AuthenticationGuard component={OrgChartPage} />}
-              />
-              <Route
-                path="/upload"
-                element={<AuthenticationGuard component={UploadPage} />}
-              />
-              <Route
-                path="/profile"
-                element={<AuthenticationGuard component={UserProfile} />}
-              />
-              <Route
-                path="/callback"
-                element={<AuthenticationGuard component={CallbackPage} />}
-              />
+							{/* private */}
+							<Route path="/home" element={<HomePage />} />
+							<Route
+								path="/org"
+								element={<AuthenticationGuard component={OrgChartPage} />}
+							/>
+							<Route
+								path="/upload"
+								element={<AuthenticationGuard component={UploadPage} />}
+							/>
+							<Route
+								path="/profile"
+								element={<AuthenticationGuard component={UserProfile} />}
+							/>
+							<Route
+								path="/callback"
+								element={<AuthenticationGuard component={CallbackPage} />}
+							/>
+							<Route
+								path="/settings"
+								element={<AuthenticationGuard component={Settings} />}
+							/>
 
-              {/* catch-all */}
-              <Route path="/*" element={<MissingPage />} />
-            </Routes>
-          </RosterUploadContext.Provider>
-        </SearchContext.Provider>
-      </SdUserContext.Provider>
-    </>
-  );
+							{/* catch-all */}
+							<Route path="/*" element={<MissingPage />} />
+						</Routes>
+					</RosterUploadContext.Provider>
+				</SearchContext.Provider>
+			</SdUserContext.Provider>
+		</>
+	);
 };
 
 export default App;
