8b361bccd86c0e7f8fb44a7cbd9200ef18febd4d
Positoxin <loki.relph@gmail.com>
2023-11-29 14:54:59 -0500

    Squad logo

--- server/seeds/03_populate_unit.js
+++ server/seeds/03_populate_unit.js
@@ -11,7 +11,7 @@ exports.seed = async function (knex) {
       unit_abbr: "VQ1234",
       unit_parent: "AF",
       unit_emblem_url:
-        "https://cfw.sarna.net/wiki/images/6/65/Federated_Suns_Logo.png",
+        "https://cdn.discordapp.com/attachments/1163519713880571954/1179505723990888458/Federated_Suns_Logo.png?ex=657a074e&is=6567924e&hm=2562972f54e26857233ad5d1f802db5382d4dc589aaf76577a70cd2fc3ce0d2f&",
       unit_station: "Vandenburg",
     },
     {
